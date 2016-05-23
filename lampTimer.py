db76c023a530404709f8b029e4b08cff6b5a6acf
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-05-22 23:04:30 -0400

    Added method to print a calcuated dusk time

--- lampTimer.py
+++ lampTimer.py
@@ -8,9 +8,21 @@ def get_astral_city():
     city = a[city_name]
     return city
 
+def print_time(time):
+    hour = time['dusk'].hour
+    minute = time['dusk'].minute
+    if minute >= 45:
+        minute = 0
+        hour += 1
+    elif minute >=15:
+        minute = 30
+    else:
+        minute = 0
+    print('Dusk: %02d:%02d' % (hour, minute))
 
 if __name__ == '__main__':
     city = get_astral_city()
     print('Information for %s/%s\n' % (city.name, city.region))
     sun = city.sun(date=datetime.datetime.now(), local=True)
     print('Dusk: %s' % str(sun['dusk']))
+    print_time(sun)

d031d154a66fb601d9cbdf5cdd018f6ae1ff9cd1
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-08-11 20:22:30 -0400

    Print out days on which to change the lamp timer

--- lampTimer.py
+++ lampTimer.py
@@ -53,6 +53,21 @@ class Month:
         for day in self.days:
             print day
 
+    def get_days_of_rounded_dusk_change(self):
+        days = []
+        last_seen_time = None
+        for day in self.days:
+            dusk = day.rounded_dusk_time().time()
+            if (dusk != last_seen_time):
+                # print 'CHANGE'
+                # print last_seen_time
+                # print "to"
+                # print dusk
+                # print "-------------------"
+                last_seen_time = dusk
+                days.append(day)
+        return days
+
 def get_astral_city():
     city_name = 'Toronto'
     a = Astral()
@@ -63,5 +78,52 @@ def get_astral_city():
 def zeroify_date(date):
     return date.replace(hour=0, minute=0, second=0, microsecond=0)
 
+def print_months(first_month, last_month, year):
+    if (first_month > last_month):
+        return
+
+    location = get_astral_city()
+    for i in range(first_month,last_month+1):
+        month = Month(datetime.datetime(year, i, 1), location)
+        month.print_as_csv()
+        print '\n'
+
+def print_days_of_rounded_dusk_change(month):
+    days = month.get_days_of_rounded_dusk_change()
+    for day in days:
+        print day
+
+def print_days_and_times_for_lamp_change(first_month, last_month, year):
+    if (first_month > last_month):
+        return
+
+    location = get_astral_city()
+    days_of_change = []
+
+    for i in range(first_month, last_month+1):
+        month = Month(datetime.datetime(year, i, 1), location)
+        days = month.get_days_of_rounded_dusk_change()
+        for day in days:
+            if (len(days_of_change) == 0):
+                days_of_change.append(day)
+                continue
+            if dusks_differ_by_one_hour(day, days_of_change[-1]):
+                days_of_change.append(day)
+
+    for day in days_of_change:
+        print day
+
+def dusks_differ_by_one_hour(day1, day2):
+    dusk1 = day1.rounded_dusk_time()
+    dusk2 = day2.rounded_dusk_time()
+
+    hour_diff = dusk1.hour - dusk2.hour
+    min_diff = dusk1.minute - dusk2.minute
+
+    return ((abs(hour_diff) == 1) and (min_diff == 0))
+
 if __name__ == '__main__':
     print 'lampTimer!'
+    # print_months(9,9)
+    print_days_and_times_for_lamp_change(9,12,2016)
+

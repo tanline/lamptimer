78bf5ff697d3312bf5cf44710afa52f3cb091aa5
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-08-28 18:05:54 -0400

    Add cli entry point and update setup file

--- lamptimer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lamptimer import print_days_and_times_for_lamp_change
-
-
-print_days_and_times_for_lamp_change(9,12,2016)
--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ setup(name='lamptimer',
       version='0.1',
       author='Tanpreet Grewal',
       author_email='tanpreet@tanpreet.com',
+      url='https://github.com/tanline/lamptimer',
       license='MIT',
-      packages=['lamptimer'],
       install_requires=[
           'Astral',
       ],
+      packages=[
+          'lamptimer',
+      ],
+      entry_points={
+          'console_scripts': [
+              'lamptimer=lamptimer.lamptimer:cli',
+          ]
+      },
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
       include_package_data=True,

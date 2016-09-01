697c082f6c89b62e38a0ea0294cc3788679f0453
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-09-01 17:40:38 -0400

    Added basic version of cli

--- lamptimer/__main__.py
+++ lamptimer/__main__.py
@@ -1,11 +1,20 @@
 import sys
 from lamptimer import print_days_and_times_for_lamp_change
 
+# Arguments are as followed:
+# lamptimer from-month to-month year
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    print args
-    print_days_and_times_for_lamp_change(9,12,2016)
+    if len(args) == 0:
+        print 'usage: lamptimer from-month to-month year'
+        return
+
+    from_month = int(args[0])
+    to_month = int(args[1])
+    year = int(args[2])
+
+    print_days_and_times_for_lamp_change(from_month,to_month,year)
 
 if __name__ == "__main__":
     main()

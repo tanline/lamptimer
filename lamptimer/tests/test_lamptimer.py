2c4811eee092e7cd3112dc27ce65a4f1508634ca
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-09-02 19:25:07 -0400

    Add test for helper method

--- lamptimer/tests/test_lamptimer.py
+++ lamptimer/tests/test_lamptimer.py
@@ -7,6 +7,10 @@ class TestLampTimer(unittest.TestCase):
     def setUp(self):
         self.location = self.get_astral_city()
 
+    def create_date(self, time):
+        time = lamptimer.zeroify_date(time)
+        return lamptimer.Day(time, self.location)
+
     @staticmethod
     def get_astral_city():
         city_name='Toronto'
@@ -15,6 +19,14 @@ class TestLampTimer(unittest.TestCase):
         found_city = a[city_name]
         return found_city
 
+    def test_dusks_differ_by_one_hour(self):
+        d1 = self.create_date(datetime.datetime(2016,01,01))
+        d2 = self.create_date(datetime.datetime(2016,01,05))
+        d3 = self.create_date(datetime.datetime(2016,01,25))
+
+        self.assertEqual(False, lamptimer.dusks_differ_by_one_hour(d1, d2))
+        self.assertEqual(False, lamptimer.dusks_differ_by_one_hour(d1, d3))
+
 
 class TestDate(TestLampTimer):
     def test_date_create(self):
@@ -22,6 +34,7 @@ class TestDate(TestLampTimer):
         newDay = lamptimer.Day(time, self.location)
         self.assertEqual(newDay.date, time)
         self.assertEqual(newDay.location, self.location)
+
 
 class TestMonth(TestLampTimer):
     def test_month_create(self):

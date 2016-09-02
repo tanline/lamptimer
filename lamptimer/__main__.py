b93a755e2b797c20f170993b77bb0876765377ff
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-09-02 16:33:08 -0400

    Added command line options using argparse

--- lamptimer/__main__.py
+++ lamptimer/__main__.py
@@ -1,20 +1,40 @@
-import sys
+import argparse
+import datetime
 from lamptimer import print_days_and_times_for_lamp_change
 
-# Arguments are as followed:
-# lamptimer from-month to-month year
 def main(args=None):
+    parser = create_parser()
+
     if args is None:
-        args = sys.argv[1:]
-    if len(args) == 0:
-        print 'usage: lamptimer from-month to-month year'
-        return
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(args)
 
-    from_month = int(args[0])
-    to_month = int(args[1])
-    year = int(args[2])
+    if not validate_date_pair(args.from_date, args.to_date):
+        msg = "The From Date must be before, or equal to, the To Date"
+        parser.error(msg)
 
-    print_days_and_times_for_lamp_change(from_month,to_month,year)
+    print args
+    # print_days_and_times_for_lamp_change(from_month,to_month,year)
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    date_help = 'a valid date in the form of YYYY-MM-DD'
+    parser.add_argument('from_date', help=date_help, type=valid_date)
+    parser.add_argument('to_date', help=(date_help + ". NOTE: date must be greater than or equal to the from_date"), type=valid_date)
+    return parser
+
+def valid_date(string):
+    try:
+        date = datetime.datetime.strptime(string, '%Y-%m-%d')
+    except ValueError:
+        msg = "%r is not a valid date. Dates should be of the form YYYY-MM-DD" % string
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return date
+
+def validate_date_pair(from_date, to_date):
+    return from_date <= to_date
 
 if __name__ == "__main__":
     main()

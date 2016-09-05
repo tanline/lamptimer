a618c45964e7c484d720575a3559b05a1d3475a2
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2016-09-05 17:19:27 -0400

    Use dictionary mapping to handle different formats

--- lamptimer/day.py
+++ lamptimer/day.py
@@ -6,19 +6,6 @@ class Day(object):
         self.location = location
         self._dusk = self._calculate_dusk_time()
 
-    def __str__(self):
-        return self.__format__()
-
-    def __format__(self, format):
-        date_str = self.date.date()
-        rounded_dusk_str = self.rounded_dusk_time().strftime('%X%z')
-
-        if format == '':
-            dusk_str = self.dusk_time().strftime('%X%z')
-            return '{}, {}, {}'.format(date_str, dusk_str, rounded_dusk_str)
-        if format == 'only-rounded':
-            return '{}, {}'.format(date_str, rounded_dusk_str)
-
     def dusk_time(self):
         return self._dusk
 
@@ -35,16 +22,34 @@ class Day(object):
             dusk_minute = 0
         return self.dusk_time().replace(hour=dusk_hour, minute=dusk_minute, second=0)
 
-    def _calculate_dusk_time(self):
-        sun = self.location.sun(date=self.date, local=True)
-        return sun['dusk']
-
     def calculate_lamp_shutoff_time(self, rounded=False, hours=4):
         on_length = datetime.timedelta(seconds=(hours*3600))
         if rounded:
             return self.rounded_dusk_time() + on_length
         else:
             return self._dusk + on_length
+
+    def __str__(self):
+        date_str = self.date.date()
+        rounded_dusk_str = self.rounded_dusk_time().strftime('%X%z')
+        dusk_str = self.dusk_time().strftime('%X%z')
+        return '{}, {}, {}'.format(date_str, dusk_str, rounded_dusk_str)
+
+    def __format__(self, format):
+        formats = {
+            'only-rounded': self._only_rounded_str
+        }
+        func = formats.get(format, self.__str__)
+        return func()
+
+    def _only_rounded_str(self):
+        date_str = self.date.date()
+        rounded_dusk_str = self.rounded_dusk_time().strftime('%X%z')
+        return '{}, {}'.format(date_str, rounded_dusk_str)
+
+    def _calculate_dusk_time(self):
+        sun = self.location.sun(date=self.date, local=True)
+        return sun['dusk']
 
     @staticmethod
     def _zeroify_date(date):

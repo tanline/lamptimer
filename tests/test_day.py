b5bb961292fa852f33241271e796fb2e4dfd5a4f
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2025-05-21 23:26:07 -0400

    Refactor tests to use london_location fixture for location initialization

--- tests/test_day.py
+++ tests/test_day.py
@@ -1,27 +1,22 @@
 from datetime import datetime
 
-from astral import LocationInfo
 from lamptimer.day import Day
 
-def test_normalize_date():
-    location = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
-
+def test_normalize_date(london_location):
     # Test with a date that has a time component
     date_with_time = datetime(2023, 10, 5, 15, 30, 45)
-    normalized_date = Day(date=date_with_time, location=location).date
+    normalized_date = Day(date=date_with_time, location=london_location).date
     assert normalized_date == datetime(2023, 10, 5, 0, 0, 0)
 
     # Test with a date that is already at midnight
     date_at_midnight = datetime(2023, 10, 5)
-    normalized_date = Day(date=date_at_midnight, location=location).date
+    normalized_date = Day(date=date_at_midnight, location=london_location).date
     assert normalized_date == datetime(2023, 10, 5, 0, 0, 0)
 
-def test_dusk_time():
-    location = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
+def test_dusk_time(london_location):
     date = datetime(2023, 10, 5)
     expected_dusk_time = datetime(2023, 10, 5, 19, 3, 28)
-
-    day = Day(date=date, location=location)
+    day = Day(date=date, location=london_location)
 
     assert day.dusk_time.date() == expected_dusk_time.date()
     assert day.dusk_time.time().hour == expected_dusk_time.hour
--- tests/test_month.py
+++ tests/test_month.py
@@ -3,24 +3,20 @@ from datetime import datetime
 from lamptimer.month import Month
 from lamptimer.day import Day
 
-from astral import LocationInfo
-
-def test_month_initialization():
-    location = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
+def test_month_initialization(london_location):
     date = datetime(2023, 10, 1)
-    month = Month(date=date, location=location)
+    month = Month(date=date, location=london_location)
 
     assert month.date == date
-    assert month.location == location
+    assert month.location == london_location
     assert len(month.days) == 31
     assert all(isinstance(day, Day) for day in month.days)
     assert month.days[0].date == datetime(2023, 10, 1)
 
-def test_month_initialization_for_leap_year():
+def test_month_initialization_for_leap_year(london_location):
     # Test for February in a leap year
-    location = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
     date = datetime(2024, 2, 1)
-    month = Month(date=date, location=location)
+    month = Month(date=date, location=london_location)
 
     assert len(month.days) == 29
     assert month.days[0].date == datetime(2024, 2, 1)

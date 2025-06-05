5192103dfbaede6ae53370d2a5e839e053ca9a91
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2025-06-04 21:26:36 -0400

    Refactor formatters and month summary to cli_output module

---
+++ src/lamptimer/cli_output/__init__.py
@@ -0,0 +1,27 @@
+from .formatters import (
+    format_location_info,
+    format_day_summary,
+    format_day_csv,
+    format_month_csv,
+    format_month_table,
+    format_day_json_data,
+    format_month_json,
+    format_month_jsonl,
+)
+from .month_summary import (
+    format_month_summary,
+    find_days_with_dusk_time_changes,
+)
+
+__all__ = [
+    "format_location_info",
+    "format_day_summary",
+    "format_day_csv",
+    "format_month_csv",
+    "format_month_table",
+    "format_day_json_data",
+    "format_month_json",
+    "format_month_jsonl",
+    "format_month_summary",
+    "find_days_with_dusk_time_changes",
+]
